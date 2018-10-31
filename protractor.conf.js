d67e188e0bd2695d03aae1e16401964ea99fc669
Julio Bachion <julio.bachion@neogrid.com>
2018-10-31 17:10:47 -0300

    Exemplo simples Protractor+cucumber
    Please enter the commit message for your changes. Lines starting

---
+++ protractor.conf.js
@@ -0,0 +1,44 @@
+exports.config = {
+  allScriptsTimeout: 11000,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--no-sandbox']
+    }
+  },
+  directConnect: true,
+  baseUrl: 'http://localhost:4200/',
+
+  // Specs here are the cucumber feature files
+  specs: [
+    './e2e/features/*.feature'
+  ],
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+  // cucumber command line options
+  cucumberOpts: {
+    // require step definition files before executing features
+    require: ['./e2e/steps/**/*.ts'],
+    // <string[]> (expression) only execute the features or scenarios with tags matching the expression
+    // tags: [],
+    // <boolean> fail if there are any undefined or pending steps
+    strict: true,
+    // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
+    format: [
+      /*'pretty',
+      'pretty:reports/summary.txt',*/
+      'json:reports/summary.json'
+    ],
+    // <boolean> invoke formatters without executing steps
+    dryRun: false,
+    // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+    compiler: []
+  },
+   // Enable TypeScript for the tests
+  onPrepare() {
+    require('ts-node').register({
+      project: 'e2e/tsconfig.e2e.json'
+    });
+   },
+  //seleniumAddress: 'http://localhost:4444/wd/hub'
+};
